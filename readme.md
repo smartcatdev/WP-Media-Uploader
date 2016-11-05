9ca6e0b68c3ed879f38880879a328e09873c88dc
bilalhassan <contact@bilalhassan.com>
2016-11-05 06:57:08 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+
+Create a media upload button button to use in your WordPress plugins or themes. 
+
+Features :
+- Easy to setup and use
+- Customizable : Make this work for your needs
+- Tiny ( less than 3 KB )
+
+## Version
+1.0
+
+## License
+
+## Basic usage
+1. HTML
+```` HTML
+<div class="form-group smartcat-uploader">
+
+	<label for="logo">Company logo</label>
+	<input type="text" name="logo">
+
+</div>
+````
---
+++ wp_media_uploader.js
@@ -0,0 +1,63 @@
+/**
+ * 
+ * 
+ */
+
+( function( $) {
+
+    $.fn.wpMediaUploader = function( options ) {
+        
+        var settings = $.extend({
+            
+            target : '.smartcat-uploader', // The class wrapping the textbox
+            uploaderTitle : 'Select or upload image', // The title of the media upload popup
+            uploaderButton : 'Set image', // the text of the button in the media upload popup
+            multiple : false, // Allow the user to select multiple images
+            buttonText : 'Upload image', // The text of the upload button
+            buttonClass : '.smartcat-upload', // the class of the upload button
+            previewSize : '150px', // The preview image size
+            modal : false, // is the upload button within a bootstrap modal ?
+            buttonStyle : { // style the button
+                color : '#fff',
+                background : '#3bafda',
+                fontSize : '16px',                
+                padding : '10px 15px',                
+            },
+
+            
+        }, options );
+        
+        
+        $( settings.target ).append( '<a href="#" class="' + settings.buttonClass.replace('.','') + '">' + settings.buttonText + '</a>' );
+        $( settings.target ).append('<div><br><img src="#" style="display: none; width: ' + settings.previewSize + '"/></div>')
+        
+        $( settings.buttonClass ).css( settings.buttonStyle );
+        
+        $('body').on('click', settings.buttonClass, function(e) {
+            
+            e.preventDefault();
+
+            var custom_uploader = wp.media({
+                title: settings.uploaderTitle,
+                button: {
+                    text: settings.uploaderButton
+                },
+                multiple: settings.multiple
+            })
+            .on('select', function() {
+                var attachment = custom_uploader.state().get('selection').first().toJSON();
+                $( settings.target + ' img').attr( 'src', attachment.url).show();
+                $( settings.target + ' input').val(attachment.url);
+                if( settings.modal ) {
+                    $('.modal').css( 'overflowY', 'auto');
+                }
+            })
+            .open();
+        });
+        
+        
+    }
+    
+})(jQuery);
+
+
